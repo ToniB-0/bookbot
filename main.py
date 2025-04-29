a1de52c635e6301af05c530b405fbd56ef8d0482
Antonio Barcellos Ometto <toniometto@gmail.com>
2025-04-29 14:28:21 -0400

    reading skills added, nano main.py made a func

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath): 
+    with open(filepath) as f:
+        text = f.read()               
+    return text
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+if __name__ == "__main__":
+    main()
