404613158c53b0cbab1142c4cdab279162e2ed11
Antonio Barcellos Ometto <toniometto@gmail.com>
2025-05-06 14:21:16 -0400

    final adjustment to stats and main files

--- stats.py
+++ stats.py
@@ -7,8 +7,3 @@ def get_num_words(text):
     words_list = text.split()
     return len(words_list)
 
-    print(f"{num_words} words found in the document")
-
-if __name__ == "__main__":
-    main()
-
