ef830b47d61732321130ea216332b261b7c951fe
Antonio Barcellos Ometto <toniometto@gmail.com>
2025-05-06 16:16:13 -0400

    more adjustments, making main.py not print debug texts

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from stats import get_num_words, book_text_read, letter_count
 
 def main():
-    print("DEBUG: main.py is being executed")
     book_text = book_text_read("books/frankenstein.txt")
     num_words = get_num_words(book_text)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
     # Get letter counts
     letter_counts = letter_count(book_text)
-    print("DEBUG: Letter counts dictionary:", letter_counts)
 
     # Check if the dictionary is empty
     if not letter_counts:
@@ -18,7 +16,6 @@ def main():
         for letter, count in sorted(letter_counts.items()):
             print(f"  {letter}: {count}")
 
-print("hello world")
 if __name__ == "__main__":
     main()
 
